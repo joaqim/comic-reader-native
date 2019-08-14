94c3b9c1c027a6d862cdc5e85a4c6829e7b54739
Joaqim Planstedt <joaqimp@gmail.com>
2019-08-14 08:35:55 +0200

    Initial Work

---
+++ components/PageList.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+
+
+class MyListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.id);
+  };
+
+  render() {
+    const textColor = this.props.selected ? 'red' : 'black';
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+      <View>
+      <Text style={{color: textColor}}>{this.props.title}</Text>
+      </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+
+export default class PageList extends Component {
+    state = {
+        selected: (new Map(): Map<string, boolean>)
+    };
+
+    _keyExtractor = (item, index) => item.key;
+
+    _onPressItem = (id: string) => {
+        // updater functions are preferred for transactional updates
+        this.setState((state) => {
+            // copy the map rather than modifying state.})
+            const selected = new Map(state.selected);
+            selected.set(id, !selected.get(id)); // toggle
+            return {selected};
+        })
+    }
+    _renderItem = ({item}) => (
+            <MyListItem
+        id={item.key}
+        onPressItem={this._onPressItem}
+        selected={!!this.state.selected.get(item.key)}
+        title={item.title}
+            />
+    );
+
+    
+    render() {
+        //console.log(this.props.navigation.state)
+        //const { data } = this.props.navigation.state.params;
+        const data = this.props.navigation.getParam('data', undefined);
+        console.log(data)
+        return (
+                <View style={styles.container}>
+                <Text>PageList</Text>
+
+
+            {data !== undefined ? <FlatList
+            data={data}
+            extraData={this.state}
+            keyExtractor={this._keyExtractor}
+            renderItem={this._renderItem}
+             /> : null}
+
+            </View>
+        );
+    }
+}
+
+                //<FlatList data={['1': 0, '2': 1, '3': 2, '4': 3, '5': 4]} renderItem={({item}) => <Text style={styles.item}>Item: {item.key} </Text>} keyExtractor={(item, index) => index.toString()} />
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    item: {
+        fontSize: 18,
+    },
+})
+
